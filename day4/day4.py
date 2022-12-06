bbbbe7b43d7dec66b147d5be6234b900c2b5df19
Mason Tuttle <76038336+Skiffy106@users.noreply.github.com>
2022-12-06 13:49:22 -0800

    Day 4 Part 2

--- day4/day4.py
+++ day4/day4.py
@@ -23,3 +23,21 @@ for i in range(len(fileArray)):
     subsetSum += 1
 
 print(subsetSum)
+
+# ---------- End of Part 1 -------------
+# ---------- Begin of Part 2 -----------
+
+subsetSum = 0
+for i in range(len(fileArray)):
+  firstDashIndex = fileArray[i].find("-")
+  commaIndex = fileArray[i].find(",")
+  secondDashIndex = fileArray[i].rfind("-")
+  # print(fileArray[i][0:firstDashIndex], " - ", fileArray[i][firstDashIndex + 1:commaIndex])
+  # print(fileArray[i][commaIndex + 1:secondDashIndex], " - ", fileArray[i][secondDashIndex + 1:len(fileArray[i])])
+  elf1 = {i for i in range(int(fileArray[i][0:firstDashIndex]), int(fileArray[i][firstDashIndex + 1:commaIndex]) + 1)}
+  elf2 = {i for i in range(int(fileArray[i][commaIndex + 1:secondDashIndex]), int(fileArray[i][secondDashIndex + 1:len(fileArray[i])]) + 1)}
+
+  if (bool(elf1 & elf2)):
+    subsetSum += 1
+
+print(subsetSum)
